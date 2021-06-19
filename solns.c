d661985f5b951225da4c357e10079b3ddba8c9aa
anaghavinod01 <84581247+anaghavinod01@users.noreply.github.com>
2021-06-19 11:48:10 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,27 +1,14 @@
 /* Enter your solutions in this file */
   
-#include<stdio.h>
-#include<cs50.h>
-
-float average_of_five_numbers(int a,int b,int c,int d,int e)
-{  
-  float avg;
-          
-            avg= (a+b+c+d+e)/5.0;
-            return avg;
+/* Average function declaration */
+float average(int a[],int n)
+{
+  int i;
+  float sum=0.0;
+  for (i=0;i<n;i++)
+  {
+    sum = sum + a[i];
   }
-  
-  int main (void)
-     {
-       int u,v,w,x,y;
-       float average;
-       u=get_int("ENTER FIRST NUMBER:");
-       v=get_int("ENTER SECOND NUMBER:");
-       w=get_int("ENTER THIRD NUMBER:");
-       x=get_int("ENTER FOURTH NUMBER:");
-       y=get_int("ENTER FIFTH NUMBER:");
-       
-       average=average_of_five_numbers(u,v,w,x,y);
-       printf("AVERAGE OF 5 NUMBERS=%.2f \n",average);
-       }
-     
+   return(sum/n);
+}
+
